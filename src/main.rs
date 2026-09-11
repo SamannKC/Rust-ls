3433031cf851e3e0879f19fd881a8eb67d20deb3
SamannKC <np03cs4a250131@heraldcollege.edu.np>
2026-09-11 06:40:21 +0545

    created functions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use colored::Colorize;
 use std::{env, fs};
 use terminal_size::{terminal_size, Width};
 
+// struct for items in directory 
 struct Items{
     name: String,
     is_dir: bool,
@@ -16,6 +17,29 @@ fn main() {
 
     let mut names: Vec<Items> = Vec::new();
 
+    // Populate the struct
+    populate(&mut names, entries);
+
+    // divide into columns for better readability
+    let max_width = names.iter()
+        .map(|item| item.name.len())
+        .max()
+        .unwrap_or(0);
+
+    let terminal_width = match terminal_size() {
+        Some((Width(width), _)) => width as usize,
+        None => 80,
+    };
+
+    let column_width = max_width + 3;
+    let columns = terminal_width/column_width;
+
+    // display the items
+    display(names, &columns, &column_width);
+
+}
+
+fn populate(names: &mut Vec<Items>, entries: fs::ReadDir){
     for entry in entries {
         let entry = entry
             .expect("Couldnt read entry");
@@ -36,19 +60,10 @@ fn main() {
         })
     }
 
-    let max_width = names.iter()
-        .map(|item| item.name.len())
-        .max()
-        .unwrap_or(0);
+}
 
-    let terminal_width = match terminal_size() {
-        Some((Width(width), _)) => width as usize,
-        None => 80,
-    };
 
-    let column_width = max_width + 3;
-    let columns = terminal_width/column_width;
-
+fn display(names: Vec<Items>, columns: &usize, column_width: &usize){
     for (i, item) in names.iter().enumerate(){
         if item.is_dir{
             print!(
@@ -66,4 +81,5 @@ fn main() {
             println!();
         }
     }
+
 }
